0d1d84e165d752e9fd5aafabcf625be86c937d8f
Soroush Pour <me@soroushjp.com>
2014-09-01 21:17:31 -0400

    Initial commit. Basic go server with negroni, logger middleware, controllers and models folders

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+
+	// "fmt" // Commented out to avoid Go errors for unused imports. Uncomment after use.
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+
+	// Commented out to avoid Go errors for empty imports. Uncomment after adding controllers/models.
+	// "github.com/soroushjp/go-rest/controllers"
+	// "github.com/soroushjp/go-rest/models"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	n := negroni.New(negroni.NewLogger())
+	n.UseHandler(router)
+
+	n.Run(":3000")
+
+}
